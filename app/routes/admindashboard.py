c4ddcba6a3ece5caf9c3f8d86e6d2cd18b4db701
stephen_adah <mailstephenreal@gmail.com>
2024-08-15 18:15:39 +0100

    FEAT: modified

--- app/routes/admindashboard.py
+++ app/routes/admindashboard.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """home module"""
 from flask import Blueprint, render_template
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 admindashboard_bp = Blueprint('admindashboard', __name__)
 
 
-# admin dashboard route
 @admindashboard_bp.route("/admindashboard", methods=['GET'], strict_slashes=False)
+@jwt_required()
 def admindashboard_page():
     """Admin dashboard page route"""
+    admin_id = get_jwt_identity() # Fetch and display data using the admin_id
     return render_template('admindashboard.html')
