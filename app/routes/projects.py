8add5fc9a4517183b7877a48afbf6740f9a8f7ef
stephen_adah <mailstephenreal@gmail.com>
2024-08-15 18:18:13 +0100

    FEAT: modified route to protected

--- app/routes/projects.py
+++ app/routes/projects.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """home module"""
 from flask import Blueprint, render_template
+from flask_jwt_extended import jwt_required, get_jwt_identity
 projects_bp = Blueprint('projects', __name__)
 
-# create projects done
 @projects_bp.route("/projects", methods=['POST'], strict_slashes=False)
+@jwt_required()
 def create_projects():
-    """get all projects done"""
+    """create projects done"""
+    admin_id = get_jwt_identity()
     return "Project post endpoint"
 
 
@@ -17,15 +19,13 @@ def get_projects():
         return render_template('projects.html')
 
 
-# update a project done
 @projects_bp.route("/projects/update/<id>") #, methods=['PUT'], strict_slashes=False)
 def update_projects(id):
-    """get all projects done"""
+    """update a projects done"""
     return f"This is update {id}"
 
 
-# delete a project done
 @projects_bp.route("/projects/delete/<id>", methods=['POST'], strict_slashes=False)
 def delete_projects(id):
-    """get all projects done"""
+    """delete a projects done"""
     return f"Project delete endpoint {id}"
