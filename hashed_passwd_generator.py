1dbdab3c8c131eb001dc536cce90205c5136c69e
stephen_adah <mailstephenreal@gmail.com>
2024-08-15 08:34:38 +0100

    FEAT: to generate hashed password for default admin creation

---
+++ hashed_passwd_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""generate hashed password using werkzeug"""
+from werkzeug.security import generate_password_hash
+# import os
+
+def hash_password_generator():
+    input_env_variable_name = input("Enter the env variable name: ")
+    env_variable_name = input_env_variable_name.upper()
+    password = input("Enter the password: ")
+
+    hashed_password = generate_password_hash(password)
+
+    with open(".env", "a") as f:
+        f.write(f"{env_variable_name}={hashed_password}")
+
+if __name__ == "__main__":
+    hash_password_generator()
