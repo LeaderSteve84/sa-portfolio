1e74b919046f97ed5fe698d9da4469586f463708
stephen_adah <mailstephenreal@gmail.com>
2024-08-24 10:46:45 +0100

    OPT: modified

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,18 +2,22 @@
 """routes package init"""
 from flask import Blueprint
 from . import home
-from . import projects
+from . import featured_projects
+from . import projects_done
 from . import tech_writings
 from . import auth
 from . import about
 from . import admindashboard
+from . import references
 
 bp = Blueprint('main', __name__)
 
 # register blueprint into main "bp"
 bp.register_blueprint(home.home_bp)
-bp.register_blueprint(projects.projects_bp)
+bp.register_blueprint(featured_projects.featured_projects_bp)
+bp.register_blueprint(projects_done.projects_done_bp)
 bp.register_blueprint(tech_writings.tech_writings_bp)
 bp.register_blueprint(auth.auth_bp)
 bp.register_blueprint(about.about_bp)
 bp.register_blueprint(admindashboard.admindashboard_bp)
+bp.register_blueprint(references.references_bp)
