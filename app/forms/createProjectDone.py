cef03511462ba3cd5d46fe9f93ccc13f45ab922c
stephen_adah <mailstephenreal@gmail.com>
2024-08-24 10:30:25 +0100

    OPT: modified

---
+++ app/forms/createProjectDone.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Optional
+
+
+class ProjectDoneForm(FlaskForm):
+    """Class to create form for adding projects Done"""
+    title = StringField('Title', validators=[DataRequired()])
+    project_type = StringField('Type e.g personal, open source etc', validators=[DataRequired()])
+    description = TextAreaField('Describe Project', validators=[DataRequired()])
+    stacks = StringField('Stacks - Technologies Used', validators=[DataRequired()])
+    role = TextAreaField('Project Role', validators=[DataRequired()])
+    date_cmptd = StringField('Completion Date', validators=[DataRequired()])
+    submit = SubmitField('Add Project Done')
