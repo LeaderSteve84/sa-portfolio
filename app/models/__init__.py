91138af9f5e9045f918b7962afe9a18c7a1cb437
stephen_adah <mailstephenreal@gmail.com>
2024-08-24 10:45:54 +0100

    FEAT: imported Reference

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from .admin import Admin
-from .project import Project
+from .featured_project import FeaturedProject
+from .project_done import ProjectDone
 from .writing import Writing
+from .reference import Reference
