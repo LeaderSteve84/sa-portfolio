2241f72078fdd9947060bdf4ffaeafecc40ac244
stephen_adah <mailstephenreal@gmail.com>
2024-09-26 10:28:40 +0100

    STYLE: Pycodestyle

--- app/routes/disclaimer_or_terms.py
+++ app/routes/disclaimer_or_terms.py
@@ -5,7 +5,11 @@ from flask import Blueprint, render_template
 disclaimer_or_terms_bp = Blueprint('disclaimer_or_terms', __name__)
 
 
-@disclaimer_or_terms_bp.route("/disclaimer-or-terms", methods=['GET'], strict_slashes=False)
+@disclaimer_or_terms_bp.route(
+    "/disclaimer-or-terms",
+    methods=['GET'],
+    strict_slashes=False
+)
 def disclaimer_or_terms_page():
     """disclaimer or terms page route"""
     return render_template('disclaimer_or_terms.html')
