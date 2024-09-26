205156ce000bd8030ad2f5ee9057caaa1e398705
stephen_adah <mailstephenreal@gmail.com>
2024-09-26 09:53:06 +0100

    STYLE: Pycodestyle

--- app/models/admin.py
+++ app/models/admin.py
@@ -9,6 +9,7 @@ from uuid import uuid4
 
 db = current_app.db
 
+
 class Admin(db.Model):
     """class of Admin table"""
     __tablename__ = 'admin'
@@ -19,12 +20,11 @@ class Admin(db.Model):
     email: Mapped[str] = mapped_column(db.String(length=30), unique=True)
     password_hash: Mapped[str] = mapped_column(db.String(length=128))
     phone: Mapped[str] = mapped_column(db.String(length=30), unique=True)
-    
-    
+
     def __repr__(self):
         return f"<admin {self.id}>"
-   
 
+    # password setter
     def set_password(self, password):
         """method to set or change admin password"""
         hash_password = generate_password_hash(password)
