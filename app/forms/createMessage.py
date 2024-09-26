dc3d929bebf052abab72c1e91669a222661f8e86
stephen_adah <mailstephenreal@gmail.com>
2024-09-26 09:20:56 +0100

    STYLE: pycodestyle

--- app/forms/createMessage.py
+++ app/forms/createMessage.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, Optional 
+from wtforms.validators import DataRequired, Email, Length, Optional
+
 
 class ContactForm(FlaskForm):
     """Class to create form for creating contact messages"""
     name = StringField('Name', validators=[DataRequired(), Length(max=100)])
-    email = StringField('Email', validators=[DataRequired(), Length(max=1000), Email()])
+    email = StringField(
+        'Email', validators=[DataRequired(), Length(max=1000), Email()]
+    )
     subject = StringField('Subject', validators=[DataRequired()])
     message = TextAreaField('Message', validators=[DataRequired()])
     status = StringField('Status', validators=[Optional()])
