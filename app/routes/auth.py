6e04852cc345ac6a269f77a86fcc1349a7f4f783
stephen_adah <mailstephenreal@gmail.com>
2024-09-26 10:14:26 +0100

    STYLE: Pycodestyle

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """home module"""
 from flask import Blueprint, request, render_template, redirect, url_for, \
-flash, jsonify, make_response, current_app
+    flash, jsonify, make_response, current_app
 from flask_jwt_extended import create_access_token, set_access_cookies, \
-unset_jwt_cookies, jwt_required, get_jwt_identity
+    unset_jwt_cookies, jwt_required, get_jwt_identity
 from app.forms.adminLogin import AdminLoginForm, AdminLogoutForm
 from app.forms.forgotPassword import ForgotPasswordForm
 from app.forms.resetPassword import ResetPasswordForm
@@ -28,10 +28,18 @@ def login():
     if form.validate_on_submit():
         username_email = form.username_email.data
         password = form.password.data
-        admin = Admin.query.filter((Admin.username == username_email) | (Admin.email == username_email)).first()
+        admin = Admin.query.filter(
+            (Admin.username == username_email)
+            |
+            (Admin.email == username_email)
+        ).first()
         if admin and check_password_hash(admin.password_hash, password):
-            access_token = create_access_token(identity=admin.id, expires_delta=timedelta(hours=1))
-            response = make_response(redirect(url_for('main.admindashboard.admindashboard_page')))
+            access_token = create_access_token(
+                identity=admin.id, expires_delta=timedelta(hours=1)
+            )
+            response = make_response(
+                redirect(url_for('main.admindashboard.admindashboard_page'))
+            )
             set_access_cookies(response, access_token)
             return response
         else:
@@ -78,7 +86,7 @@ def verify_reset_token(token, expiration=3600):
     s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
     try:
         email = s.loads(token, salt="password-reset-salt", max_age=expiration)
-    except:
+    except exception as e:
         return None
     return email
 
@@ -89,7 +97,9 @@ def send_reset_email(admin_email, token):
         subject='Password Reset Request',
         recipients=[admin_email]
     )
-    reset_url = url_for('main.auth.reset_password', token=token, _external=True)
+    reset_url = url_for(
+        'main.auth.reset_password', token=token, _external=True
+    )
     msg.body = f'''To reset your password, visit the following link:
     {reset_url}
     If you did not make this request, simply ignore this email.
@@ -161,7 +171,9 @@ def change_password(admin_id):
             # check if old password is correct
             if not check_password_hash(admin.password_hash, old_password):
                 flash('Incorrect current password', 'warning')
-                return render_template('change_password.html', form=form, admin=admin)
+                return render_template(
+                    'change_password.html', form=form, admin=admin
+                )
             admin.set_password(new_password)
             db.session.commit()
             flash('Admin password updated successfully', 'success')
@@ -169,5 +181,7 @@ def change_password(admin_id):
     else:
         if form.errors != {}:
             for error_message in form.errors.values():
-                flash(f"Error changing your password: {error_message}", "error")
+                flash(
+                    f"Error changing your password: {error_message}", "error"
+                )
                 return redirect(url_for('main.auth.change_password_page'))
