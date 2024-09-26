e6df89da36ec3b103acbfd831236122c196402c7
stephen_adah <mailstephenreal@gmail.com>
2024-09-26 09:45:09 +0100

    STYLE: Pycodestyle

--- app/forms/resetPassword.py
+++ app/forms/resetPassword.py
@@ -2,8 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
 
+
 class ResetPasswordForm(FlaskForm):
     """class fot reset passform form"""
     password = PasswordField('New Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField(
+        'Confirm Password', validators=[DataRequired(), EqualTo('password')]
+    )
     submit = SubmitField('Reset Password')
