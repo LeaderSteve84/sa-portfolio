a982a97cb2bb12e200795a950cbf97bf85d1b573
stephen_adah <mailstephenreal@gmail.com>
2024-09-26 10:41:46 +0100

    STYLE: Pycodestyle

--- app/routes/privacy_policy.py
+++ app/routes/privacy_policy.py
@@ -5,7 +5,9 @@ from flask import Blueprint, render_template
 privacy_policy_bp = Blueprint('privacy_policy', __name__)
 
 
-@privacy_policy_bp.route("/privacy-policy", methods=['GET'], strict_slashes=False)
+@privacy_policy_bp.route(
+    "/privacy-policy", methods=['GET'], strict_slashes=False
+)
 def privacy_policy_page():
     """privacy policy page route"""
     return render_template('privacy_policy.html')
