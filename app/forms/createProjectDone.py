7d8abed2510d498709ea58105d575fc9d96d6d53
stephen_adah <mailstephenreal@gmail.com>
2024-09-26 09:22:59 +0100

    STYLE: pycodestyle

--- app/forms/createProjectDone.py
+++ app/forms/createProjectDone.py
@@ -6,9 +6,15 @@ from wtforms.validators import DataRequired, Optional
 class ProjectDoneForm(FlaskForm):
     """Class to create form for adding projects Done"""
     title = StringField('Title', validators=[DataRequired()])
-    project_type = StringField('Type e.g personal, open source etc', validators=[DataRequired()])
-    description = TextAreaField('Describe Project', validators=[DataRequired()])
-    stacks = StringField('Stacks - Technologies Used', validators=[DataRequired()])
+    project_type = StringField(
+        'Type e.g personal, open source etc', validators=[DataRequired()]
+    )
+    description = TextAreaField(
+        'Describe Project', validators=[DataRequired()]
+    )
+    stacks = StringField(
+        'Stacks - Technologies Used', validators=[DataRequired()]
+    )
     role = TextAreaField('Project Role', validators=[DataRequired()])
     date_cmptd = StringField('Completion Date', validators=[DataRequired()])
     video_link = StringField('Video URL (Optional)', validators=[Optional()])
